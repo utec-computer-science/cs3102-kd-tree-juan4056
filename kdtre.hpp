6512f2432168a677f4c6d2cc7db76dbc740f8d7a
juan4056 <juan.vargas@utec.edu.pe>
2020-03-30 00:53:11 -0500

    feat (insert): Implement function to insert new node in kdtree

--- kdtre.hpp
+++ kdtre.hpp
@@ -2,6 +2,7 @@
 #define kdtree
 
 #include <bits/stdc++.h>
+#include <iostream>
 
 const int k = 2;
 
@@ -13,9 +14,9 @@ private:
         Any coor [k];
         Node * r;
         Node * l;
-    }
+    };
 
-    Node * root;
+    Node * root = nullptr;
 
 public:
     kdtre() = default;
@@ -23,7 +24,46 @@ public:
 
     void insert (Any * coor)
     {
+        Node * newNode = new Node;
+        newNode->r = nullptr;
+        newNode->l = nullptr;
 
+        for (int i = 0; i < k; i++){
+            newNode->coor[i] = coor [i];
+        }
+
+        //std::cout <<"\nINSERT " << coor [0] << " " << coor [1] << "\n";
+
+        if (root) {
+            int t = 0;
+            Node * temp = root;
+            while(true){
+                //std::cout << "paso por " << temp->coor [0] << " " << temp->coor [1];
+                if (temp->coor [t % k] <= coor [t % k]){
+                    //std :: cout << " right \n";
+                    if (temp->r)
+                        temp = temp->r;
+                    else{
+                        temp->r = newNode;
+                        break;
+                    }
+                }
+                else{
+                    //std::cout << " left \n";
+                    if (temp->l)
+                        temp = temp->l;
+                    else{
+                        temp->l = newNode;
+                        break;
+                    }
+                }          
+
+                t++;    
+            }
+
+        }
+        else
+            root = newNode;
     }
 
     void search (Any * coor)
