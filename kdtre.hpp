d7cb78ee0aa5b6060449af6454fdcae252fc8548
juan4056 <juan.vargas@utec.edu.pe>
2020-03-29 23:42:24 -0500

    feat(node): Add node struct

--- kdtre.hpp
+++ kdtre.hpp
@@ -9,12 +9,27 @@ template <class Any>
 class kdtre
 {
 private:
+    struct Node {
+        Any coor [k];
+        Node * r;
+        Node * l;
+    }
+
+    Node * root;
+
 public:
     kdtre() = default;
     ~kdtre() = default;
 
-    void insert ();
-    void search ();
+    void insert (Any * coor)
+    {
+
+    }
+
+    void search (Any * coor)
+    {
+
+    }
 
 };
 
