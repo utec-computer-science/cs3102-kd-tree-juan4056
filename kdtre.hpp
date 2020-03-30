679eb68932ddd66984fb314d3ea605b589d0bb07
juan4056 <juan.vargas@utec.edu.pe>
2020-03-29 22:56:45 -0500

    feat: Implement base for

---
+++ kdtre.hpp
@@ -0,0 +1,23 @@
+#ifndef kdtree
+#define kdtree
+
+#include <bits/stdc++.h>
+
+const int k = 2;
+
+template <class Any>
+class kdtre
+{
+private:
+public:
+    kdtre() = default;
+    ~kdtre() = default;
+
+    void insert ();
+    void search ();
+
+};
+
+
+
+#endif
